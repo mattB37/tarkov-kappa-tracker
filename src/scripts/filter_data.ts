83c40a67497d3ea5ca8b571bf3b02d1f85ba1f32
Matthew Bosch <matthew.bosch37@gmail.com>
2025-06-03 11:44:33 -0700

    save progress

--- src/scripts/filter_data.ts
+++ src/scripts/filter_data.ts
@@ -1,10 +1,12 @@
 import rawData from '../data/raw-task-data.json' with {type: "json"};
-import type { TaskData } from './types';
+import hideoutRawData from '../data/raw-hideout-data.json' with {type: "json"}
+import type { TaskData, HideoutData } from './types';
 import { writeFilteredJSONDataToFile, getUniqueTraderNames, createTaskTrackerData, createItemTrackerData, createHideoutItemTrackerData } from './utils.ts';
 
 const taskData = JSON.parse(JSON.stringify(rawData)).data as TaskData;
 const kappaTasks = taskData.tasks.filter(task => task.kappaRequired);
 const traderNames = getUniqueTraderNames(kappaTasks);
+const hideoutData = JSON.parse(JSON.stringify(hideoutRawData)).data as HideoutData;
 
 const filteredTraderTaskData = createTaskTrackerData(traderNames, kappaTasks);
 writeFilteredJSONDataToFile('filtered_trader_task_data.json', filteredTraderTaskData);
@@ -12,5 +14,5 @@ writeFilteredJSONDataToFile('filtered_trader_task_data.json', filteredTraderTask
 const filteredItemTrackerData = createItemTrackerData(kappaTasks);
 writeFilteredJSONDataToFile('filtered_item_data.json', filteredItemTrackerData);
 
-const filteredHideoutTrackerData = createHideoutItemTrackerData();
+const filteredHideoutTrackerData = createHideoutItemTrackerData(hideoutData);
 writeFilteredJSONDataToFile('filtered_hideout_data.json', filteredHideoutTrackerData);
